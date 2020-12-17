95ed747af46d82109f4e6806764d6a99de0e6411
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2020-12-18 01:36:12 +0330

    0.0.6

--- app/src/main/java/com/example/rostmanafinal/MainActivity.java
+++ app/src/main/java/com/example/rostmanafinal/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.rostmanafinal.Fragments.FirstFragment;
+import com.example.rostmanafinal.Fragments.FragmentHome;
 import com.example.rostmanafinal.Fragments.FragmentViewPagerTabbed;
 import com.example.rostmanafinal.Fragments.Fragment_Plant_Details;
 import com.example.rostmanafinal.Fragments.SecondFragment;
@@ -24,45 +25,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationBottom);
+        bottomNavigationView.setSelectedItemId(R.id.thirdFragment);
+
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.firstFragment:
                         Toast.makeText(MainActivity.this, "Left", Toast.LENGTH_SHORT).show();
+                        FragmentManager fragmentManager2 = getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction2 = fragmentManager2.beginTransaction();
+                        SecondFragment fn2 = new SecondFragment();
+                        fragmentTransaction2.replace(R.id.nav_host_fragment, fn2);
+                        fragmentTransaction2.commit();
+//                        return true;
+                        break;
+                    case R.id.secondFragment:
+                        Toast.makeText(MainActivity.this, "Middle", Toast.LENGTH_SHORT).show();
                         FragmentManager fragmentManager = getSupportFragmentManager();
                         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                         FragmentViewPagerTabbed fn = new FragmentViewPagerTabbed();
                         fragmentTransaction.replace(R.id.nav_host_fragment, fn);
                         fragmentTransaction.commit();
-
-                        return true;
-                    case R.id.secondFragment:
-
-
-                        Toast.makeText(MainActivity.this, "Middle", Toast.LENGTH_SHORT).show();
-                        FragmentManager fragmentManager2 = getSupportFragmentManager();
-                        FragmentTransaction fragmentTransaction2 = fragmentManager2.beginTransaction();
-                        SecondFragment fn2 = new SecondFragment();
-                        fragmentTransaction2.replace(R.id.nav_host_fragment, fn2);
-
-                        fragmentTransaction2.commit();
-
                         break;
                     case R.id.thirdFragment:
                         Toast.makeText(MainActivity.this, "Right", Toast.LENGTH_SHORT).show();
                         FragmentManager fragmentManager3 = getSupportFragmentManager();
                         FragmentTransaction fragmentTransaction3 = fragmentManager3.beginTransaction();
-                        Fragment_Plant_Details fn3 = new Fragment_Plant_Details();
+                        FragmentHome fn3 = new FragmentHome();
                         fragmentTransaction3.replace(R.id.nav_host_fragment, fn3);
-
                         fragmentTransaction3.commit();
                         break;
                 }
                 return true;
             }
         });
-
 
 
     }
