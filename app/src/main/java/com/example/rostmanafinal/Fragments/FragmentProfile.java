64e9cf7896442a7b385f83ce3045db36e5609577
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2021-01-19 17:20:26 +0330

    0.3.2 Edit Profile UI going for the code

--- app/src/main/java/com/example/rostmanafinal/Fragments/FragmentProfile.java
+++ app/src/main/java/com/example/rostmanafinal/Fragments/FragmentProfile.java
@@ -1,6 +1,15 @@
 package com.example.rostmanafinal.Fragments;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +26,18 @@ import androidx.navigation.Navigation;
 
 import com.example.rostmanafinal.MainActivity;
 import com.example.rostmanafinal.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import static android.app.Activity.RESULT_OK;
 
 public class FragmentProfile extends Fragment {
-    ImageView imageViewReturn;
+    ImageView imageViewReturn, circularImageView2;
     Button btn_male, btn_female;
+    MaterialButtonToggleGroup materialButtonToggleGroup;
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
 
     @Nullable
     @Override
@@ -33,24 +50,56 @@ public class FragmentProfile extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        btn_male = view.findViewById(R.id.btn_male);
-        btn_female = view.findViewById(R.id.btn_female);
         imageViewReturn = view.findViewById(R.id.imageViewReturn);
-        btn_male.setOnClickListener(new View.OnClickListener() {
+        circularImageView2 = view.findViewById(R.id.circularImageView2);
+
+
+//        circularImageView2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//                startActivityForResult(intent, 0);
+//
+//            }
+//        });
+
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Choose your profile picture");
+
+        circularImageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btn_female.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.lGreen2OnSelected));
-                btn_male.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.lGreen2));
-            }
-        });
-        btn_female.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btn_male.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.lGreen2OnSelected));
-                btn_female.setBackgroundColor(ContextCompat.getColor(requireActivity(), R.color.lGreen2));
+
+
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+
+                        if (options[item].equals("Take Photo")) {
+                            Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                            startActivityForResult(takePicture, 0);
+//circularImageView2.setImageResource();
+                        } else if (options[item].equals("Choose from Gallery")) {
+//                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                    startActivityForResult(pickPhoto , 1);
+                            openGallery();
+
+
+                        } else if (options[item].equals("Cancel")) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();
             }
         });
 
+
+        materialButtonToggleGroup = view.findViewById(R.id.toggleButton);
+        int buttonId = materialButtonToggleGroup.getCheckedButtonId();
+        MaterialButton button = materialButtonToggleGroup.findViewById(buttonId);
 
         imageViewReturn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,5 +108,19 @@ public class FragmentProfile extends Fragment {
             }
         });
 
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageUri = data.getData();
+            circularImageView2.setImageURI(imageUri);
+        }
     }
 }
