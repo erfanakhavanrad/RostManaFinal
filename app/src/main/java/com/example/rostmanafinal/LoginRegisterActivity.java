0a8d1cbfa1268cab6c68a6f66d6544203ac57185
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2021-01-26 17:05:36 +0330

    0.4.9 Wrote POST method
    Registered just edit input
    Loading is ok

--- app/src/main/java/com/example/rostmanafinal/LoginRegisterActivity.java
+++ app/src/main/java/com/example/rostmanafinal/LoginRegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.rostmanafinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -9,6 +10,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -18,8 +20,10 @@ import android.text.Html;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rostmanafinal.Fragments.FirstFragment;
@@ -43,13 +47,39 @@ public class LoginRegisterActivity extends AppCompatActivity {
     private String TAG;
     Users users;
     Boolean number = true;
+    private Context context;
     boolean doubleBackToExitPressedOnce = false;
     ConstraintLayout constraintProgress;
+    private int duration = Toast.LENGTH_LONG;
+    TextView txtForgotPassword;
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register);
+        txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.custom_toast, cardView);
+
+                TextView text = layout.findViewById(R.id.text);
+                text.setText(R.string.custom_toast_message);
+
+                Toast toast = new Toast(LoginRegisterActivity.this);
+                toast.setGravity(Gravity.BOTTOM, 0, 40);
+                toast.setDuration(duration);
+                toast.setView(layout);
+                toast.show();
+
+            }
+        });
+
+
         constraintProgress = findViewById(R.id.constraintProgress);
         btnRegister = findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
